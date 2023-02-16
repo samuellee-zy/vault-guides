3a027aad2945a48b23fd7fe8dfe444b740387002
samuellee-zy <samuelee.zy@gmail.com>
2023-02-17 08:40:46 +1100

    Edited outputs

--- identity/vault-agent-templates/terraform-aws/outputs.tf
+++ identity/vault-agent-templates/terraform-aws/outputs.tf
@@ -7,11 +7,11 @@ Vault Server IP (private): ${join(", ", aws_instance.vault-server.*.private_ip)}
 For example:
    ssh -i ${var.key_name}.pem ubuntu@${aws_instance.vault-server[0].public_ip}
 
-Vault Client IP (public):  ${aws_instance.vault-client.public_ip}
-Vault Client IP (private): ${aws_instance.vault-client.private_ip}
+Vault Client IP (public):  ${aws_instance.vault-client[count.index].public_ip}
+Vault Client IP (private): ${aws_instance.vault-client[count.index].private_ip}
 
 For example:
-   ssh -i ${var.key_name}.pem ubuntu@${aws_instance.vault-client.public_ip}
+   ssh -i ${var.key_name}.pem ubuntu@${aws_instance.vault-client[count.index].public_ip}
    
 EOF
 
