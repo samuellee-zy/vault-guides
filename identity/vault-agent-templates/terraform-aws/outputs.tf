2a2499e17f980aca3eaf2d676a9991a9014fffef
samuellee-zy <samuelee.zy@gmail.com>
2023-02-17 08:42:26 +1100

    Included 2nd output

--- identity/vault-agent-templates/terraform-aws/outputs.tf
+++ identity/vault-agent-templates/terraform-aws/outputs.tf
@@ -1,4 +1,4 @@
-output "endpoints" {
+output "endpoints_1" {
   value = <<EOF
 
 Vault Server IP (public):  ${join(", ", aws_instance.vault-server.*.public_ip)}
@@ -7,11 +7,30 @@ Vault Server IP (private): ${join(", ", aws_instance.vault-server.*.private_ip)}
 For example:
    ssh -i ${var.key_name}.pem ubuntu@${aws_instance.vault-server[0].public_ip}
 
-Vault Client IP (public):  ${aws_instance.vault-client[count.index].public_ip}
-Vault Client IP (private): ${aws_instance.vault-client[count.index].private_ip}
+Vault Client IP (public):  ${aws_instance.vault-client[0].public_ip}
+Vault Client IP (private): ${aws_instance.vault-client[0].private_ip}
 
 For example:
-   ssh -i ${var.key_name}.pem ubuntu@${aws_instance.vault-client[count.index].public_ip}
+   ssh -i ${var.key_name}.pem ubuntu@${aws_instance.vault-client[0].public_ip}
+   
+EOF
+
+}
+
+output "endpoints_2" {
+  value = <<EOF
+
+Vault Server IP (public):  ${join(", ", aws_instance.vault-server.*.public_ip)}
+Vault Server IP (private): ${join(", ", aws_instance.vault-server.*.private_ip)}
+
+For example:
+   ssh -i ${var.key_name}.pem ubuntu@${aws_instance.vault-server[0].public_ip}
+
+Vault Client IP (public):  ${aws_instance.vault-client[1].public_ip}
+Vault Client IP (private): ${aws_instance.vault-client[1].private_ip}
+
+For example:
+   ssh -i ${var.key_name}.pem ubuntu@${aws_instance.vault-client[1].public_ip}
    
 EOF
 
